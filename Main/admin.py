a9679ce502a8fe98d9959a323b2e79f12b5fd81c
Bruno Martins <88806986+BrunoMartinsGameDev@users.noreply.github.com>
2023-10-17 14:19:04 -0300

    Models, incomplete

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Tech)
+class TechAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    search_fields = ['nome']
+    
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+    list_filter = ['createdDate', 'updatedDate']
+
--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+
+class Tech(models.Model):
+    nome = models.CharField(max_length=255,unique=True)
+    imagem = models.ImageField(upload_to='tech')
+
+    def __str__(self) -> str:
+        return self.nome
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    tecnologias = models.ForeignKey(Tech, on_delete=models.DO_NOTHING)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='project')
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
