d5e109f0cb2c41a68d65d80f90264fa55c6658bb
Bruno Martins <88806986+BrunoMartinsGameDev@users.noreply.github.com>
2025-07-29 11:26:13 -0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import { type Project } from './ProjectCard';
 export const projects: Project[] = [
   {
     title: 'Portfolio Moderno',
-    image: '/projects/portfolio/mainImage.png',
+    image: 'projects/portfolio/mainImage.png',
     description: 'Site pessoal com design responsivo e tons neutros.',
     details: 'Este projeto foi desenvolvido com React e Vite, focando em UI/UX moderno e deploy no GitHub Pages.',
     repo: 'https://github.com/BrunoMartinsGameDev/Portfolio',
@@ -11,7 +11,7 @@ export const projects: Project[] = [
   },
   {
     title: 'Sorteador de Equipes',
-    image: '/projects/sorteador/mainImage.png',
+    image: 'projects/sorteador/mainImage.png',
     description: 'Um aplicativo web moderno para criar, sortear e organizar equipes de forma dinâmica',
     details: 'Um aplicativo web moderno para criar, sortear e organizar equipes de forma dinâmica, visual e intuitiva. Ideal para projetos, hackathons, trabalhos em grupo, eventos e qualquer situação em que seja necessário dividir pessoas em equipes equilibradas. Feito com html, css e js vanilla, script único',
     repo: 'https://github.com/BrunoMartinsGameDev/Sorteador-de-Equipes',
@@ -19,7 +19,7 @@ export const projects: Project[] = [
   },
   {
     title: 'Falling Sand',
-    image: '/projects/fallingsand/mainImage.png',
+    image: 'projects/fallingsand/mainImage.png',
     description: 'Um simulador interativo de areia caindo feito em Python com pygame.',
     details: 'Simulador de partículas de areia caindo, onde o usuário pode interagir adicionando blocos de areia que se comportam de forma realista. Ideal para estudos de simulação física simples e experimentação visual. Desenvolvido em Python puro utilizando a biblioteca pygame.',
     repo: 'https://github.com/BrunoMartinsGameDev/FallingSand',
@@ -27,7 +27,7 @@ export const projects: Project[] = [
   },
   {
     title: 'Prevention Time',
-    image: '/projects/preventiontime/mainImage.png',
+    image: 'projects/preventiontime/mainImage.png',
     description: 'Primeiro Projeto feito na minha carreira de programação, um advergame sobre prevenção a gripe',
     details: 'Advergame desenvolvido para conscientizar sobre a prevenção da gripe, utilizando Unity e C#. O jogo é uma experiência interativa que combina aprendizado e diversão, ideal para campanhas educativas. O médico deve coletar vacinas, mascaras e álcool em gel espalhados pelo cenário e entrega-los para os cidadãos, evitando que eles sejam infectados pelo vírus da gripe.',
     repo: '',
@@ -35,7 +35,7 @@ export const projects: Project[] = [
   },
   {
     title: 'Space Shooter',
-    image: '/projects/spaceshooter/mainImage.png',
+    image: 'projects/spaceshooter/mainImage.png',
     description: 'Segundo Projeto feito na minha carreira de programação, um jogo de nave espacial',
     details: 'Jogo de nave espacial desenvolvido com Unity e C#. O jogador controla uma nave e deve destruir asteróides que vem em sua direção, evitando ser destruido, um infinity runner que desafia o jogador a sobreviver o máximo possível.',
     repo: '',
@@ -43,7 +43,7 @@ export const projects: Project[] = [
   },
   {
     title: "Templar's Choices",
-    image: '/projects/templarschoices/mainImage.png',
+    image: 'projects/templarschoices/mainImage.png',
     description: 'Meu TCC da faculdade de Jogos Digitais, um jogo de aventura narrativa onde suas escolhas moldam a história.',
     details: 'Em Templar\'s Choices, você assume o papel de um templário que recebe ordens da igreja devota ao Deus Greed. Suas decisões — obedecer ou desafiar as ordens — impactam profundamente o rumo da narrativa, levando a cinco finais totalmente distintos. Cada escolha traz consequências únicas, proporcionando uma experiência envolvente e cheia de reviravoltas. Descubra todos os desfechos e divirta-se explorando os diferentes caminhos dessa jornada épica.',
     repo: '',
